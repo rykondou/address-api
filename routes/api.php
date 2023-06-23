57726ed04ba53c75717acdb39972af1d057b9f75
rykondou <116945927+rykondou@users.noreply.github.com>
2023-06-23 10:58:38 +0900

    例外処理実装

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostalCodeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZipCodeController;
@@ -15,7 +16,7 @@ use App\Http\Controllers\ZipCodeController;
 |
 */
 
-Route::get('/zip-code/{code}', [ZipCodeController::class, 'search']);
+Route::get('/search', [PostalCodeController::class, 'search']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
